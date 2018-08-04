9ab461f8d2722faa6573aeed8c98b5cdfa40d4ed
Clark <clrksrvn@gmail.com>
2018-08-04 21:27:06 +0700

    no message

--- TheCRAFTPâtisserie/Controllers/HomeController.cs
+++ TheCRAFTPâtisserie/Controllers/HomeController.cs
@@ -12,21 +12,25 @@ namespace TheCRAFTPâtisserie.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Title"] = "The Craft";
             return View();
         }
 
         public IActionResult Menu()
         {
+            ViewData["Title"] = "Menu";
             return View();
         }
 
         public IActionResult About()
         {
+            ViewData["Title"] = "About";
             return View();
         }
 
         public IActionResult Workshop()
         {
+            ViewData["Title"] = "Workshop";
             return View();
         }
 
