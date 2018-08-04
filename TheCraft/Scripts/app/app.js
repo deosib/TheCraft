f93f4ba54ed08534d50d2a57e35e674cf596acec
Clark <clrksrvn@gmail.com>
2018-08-04 17:09:33 +0700

    no message

---
+++ TheCraft/Scripts/app/app.js
@@ -0,0 +1,29 @@
+﻿var Global = Global || {
+    VER: "?v=0.1",
+    API: "//localhost/api",
+    MVC: "//localhost/craft",
+    CurrentTab: 0,
+    MaxTab: 1
+};
+
+Global.Init = function () {
+    $("#banner-slider").owlCarousel({
+        items: 4,
+        autoplay: true,
+        autoPlaySpeed: 3000,
+        autoPlayTimeout: 3000,
+        autoplayHoverPause: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+}
